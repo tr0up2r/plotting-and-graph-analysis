87b48edacdf0e5a9c7e734df6dad9aa02d6ce844
tr0up2r <kmydtr@gmail.com>
2021-12-14 12:09:07 +0900

    일부 data로 community 구성, lda topic modeling 진행할 수 있도록 코드 수정.

--- community_fastgreedy_is_until_november.py
+++ community_fastgreedy_is_until_november.py
@@ -2,13 +2,13 @@ import igraph
 import pandas as pd
 import csv
 
-df = pd.read_csv('mentor_mentee_relationship.csv')
+df = pd.read_csv('mentor_mentee_relationship_until_november.csv')
 
-data = df[['mentor', 'mentee', 'sum_es_score']]
+data = df[['mentor', 'mentee', 'sum_is_score']]
 
 # 그래프 생성
 tuples = [tuple(x) for x in data.values]
-g = igraph.Graph.TupleList(tuples, directed=True, edge_attrs=['sum_es_score'])
+g = igraph.Graph.TupleList(tuples, directed=True, edge_attrs=['sum_is_score'])
 
 g.to_undirected(mode='collapse', combine_edges='sum')
 
@@ -16,7 +16,7 @@ dendrogram = g.community_fastgreedy()
 clusters = dendrogram.as_clustering()
 membership = clusters.membership
 
-writer = csv.writer(open("community_fastgreedy_es.csv", "w", newline=''))
+writer = csv.writer(open("community_fastgreedy_is_until_november.csv", "w", newline=''))
 writer.writerow(['node_id', 'community_id'])
 for name, membership in zip(g.vs["name"], membership):
     writer.writerow([name, membership])
