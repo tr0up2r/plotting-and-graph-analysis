b8fd6a1f5bfc03795623b14060ef84d97e06841b
tr0up2r <kmydtr@gmail.com>
2022-01-27 14:18:08 +0900

    folder 분리, input output path 수정.

--- custom_library/connect_to_db.py
+++ custom_library/connect_to_db.py
@@ -3,7 +3,7 @@ import pymysql
 import csv
 
 def make_connection(cn_csv):
-    cn = pd.read_csv('connection.csv')
+    cn = pd.read_csv('../custom_library/connection.csv')
     connection = pymysql.connect(host=cn['host'][0], port=int(cn['port'][0]),
                                  user=cn['user'][0], password=cn['password'][0],
                                  db=cn['db'][0], charset=cn['charset'][0])
